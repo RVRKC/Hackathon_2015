6c691355fe6e8cf1c8223e9b7af111d3108b8dc6
RVRKC <vrayaprolu@gmail.com>
2015-09-09 05:27:11 -0400

    removed extra info

--- application/config/app-shiftexec.php
+++ application/config/app-shiftexec.php
@@ -1,11 +1,17 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /* base stuff */
-$config['nts_app_title'] = 'ShiftExec';
-$config['nts_app_url'] = 'http://www.shiftexec.com';
+#$config['nts_app_title'] = 'ShiftExec';
+#$config['nts_app_url'] = 'http://www.shiftexec.com';
 
-$config['nts_promo_url'] = 'http://www.shiftexec.com/order/';
-$config['nts_promo_title'] = 'ShiftExec Pro';
+#$config['nts_promo_url'] = 'http://www.shiftexec.com/order/';
+#$config['nts_promo_title'] = 'ShiftExec Pro';
+
+$config['nts_app_title'] = '';
+$config['nts_app_url'] = '';
+
+$config['nts_promo_url'] = '';
+$config['nts_promo_title'] = '';
 
 $config['nts_track_setup'] = '2:2';
 
